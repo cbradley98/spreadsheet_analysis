0301279e3c92567d30699459091971fa590e3986
cbradley98 <celinekb98@gmail.com>
2020-04-29 19:00:47 +0100

    Requirements

--- stage_1.py
+++ stage_1.py
@@ -1,1 +1,15 @@
-#This will be the start of the code
+import csv
+
+monthly_sales = []
+
+with open('sales.csv', 'r') as csv_file:
+    spreadsheet = csv.DictReader(csv_file)
+    for row in spreadsheet:
+        sales = int(row['sales'])
+        monthly_sales.append(sales)
+
+total_sales = 0
+for month in monthly_sales:
+    total_sales = total_sales + month
+
+print('Total sales: {}'.format(total_sales))
